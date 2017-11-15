b7e25220c6406887f31a1547ec2575c0a838d5c1
Terkal Robert <terkalma@gmail.com>
2017-11-14 22:34:07 -0500

    feat: Add Shelf and Book components
    Replaced static html with component.
    Use getAll method to render shelves

---
+++ src/Shelf.js
@@ -0,0 +1,23 @@
+import React, { Component } from 'react'
+import Book from './Book'
+
+class Shelf extends Component {
+  render () {
+    return (
+      <div className="bookshelf">
+        <h2 className="bookshelf-title">{this.props.name}</h2>
+        <div className="bookshelf-books">
+          <ol className="books-grid">
+            {this.props.books.map((book, index) => (
+              <li key={index}>
+                <Book data={book}/>
+              </li>
+            ))}
+          </ol>
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Shelf
